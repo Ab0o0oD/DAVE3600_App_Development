49d3ada371b5c735823b70acc6fd99531cb7ec2a
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-26 17:29:40 +0200

    Implemented logging and instance saving

--- ButtonClickApp/app/src/main/java/com/fredrikpedersen/buttonclickapp/MainActivity.java
+++ ButtonClickApp/app/src/main/java/com/fredrikpedersen/buttonclickapp/MainActivity.java
@@ -1,9 +1,11 @@
 package com.fredrikpedersen.buttonclickapp;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.text.method.ScrollingMovementMethod;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -11,24 +13,88 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText userInput;
-    private Button button;
     private TextView textView;
+    private static final String TAG = "MainActivity";
+    private final String TEXT_CONTENTS = "TextContents";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+        Log.d(TAG, "onCreate: called");
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //Initializing the views
         userInput = findViewById(R.id.editText);
-        button = findViewById(R.id.button);
+        Button button = findViewById(R.id.button);
         textView = findViewById(R.id.textView);
 
-        textView.setText(""); //making sure the TextView is empty at program start.
+        //Setting initial view behaviour
         textView.setMovementMethod(new ScrollingMovementMethod());
 
-        button.setOnClickListener(view -> { //OnClickListener for our button, appends text to textView
+        button.setOnClickListener(view -> { //Appends text from userInput to textView every time the button is clicked
             String input = userInput.getText().toString() + "\n";
             textView.append(input);
+            userInput.setText("");
         });
+
+        Log.d(TAG, "onCreate: done");
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d(TAG, "onStart: called");
+        super.onStart();
+        Log.d(TAG, "onStart: done");
+    }
+
+    @Override
+    protected void onRestart() {
+        Log.d(TAG, "onRestart: called");
+        super.onRestart();
+        Log.d(TAG, "onRestart: done");
+    }
+
+    @Override
+    protected void onResume() {
+        Log.d(TAG, "onResume: called");
+        super.onResume();
+        Log.d(TAG, "onResume: done");
+    }
+
+    @Override
+    protected void onPause() {
+        Log.d(TAG, "onPause: called");
+        super.onPause();
+        Log.d(TAG, "onPause: done");
+    }
+
+    @Override
+    protected void onStop() {
+        Log.d(TAG, "onStop: called");
+        super.onStop();
+        Log.d(TAG, "onStop: done");
+    }
+
+    @Override
+    protected void onDestroy() {
+        Log.d(TAG, "onDestroy: called");
+        super.onDestroy();
+        Log.d(TAG, "onDestroy: done");
+    }
+
+    @Override
+    protected void onRestoreInstanceState(@NonNull Bundle savedInstanceState) {
+        Log.d(TAG, "onRestoreInstanceState: called");
+        super.onRestoreInstanceState(savedInstanceState);
+        textView.setText(savedInstanceState.getString(TEXT_CONTENTS));
+        Log.d(TAG, "onRestoreInstanceState: done");
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        Log.d(TAG, "onSaveInstanceState: called");
+        outState.putString(TEXT_CONTENTS, textView.getText().toString());
+        super.onSaveInstanceState(outState);
+        Log.d(TAG, "onSaveInstanceState: done");
     }
 }
