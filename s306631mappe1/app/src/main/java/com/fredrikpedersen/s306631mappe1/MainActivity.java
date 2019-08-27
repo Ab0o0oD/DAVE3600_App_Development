2c7ee572aaf0e490f811eeb83a8a228087546565
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-27 14:41:56 +0200

    Finnished layput for stats_view

--- s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/MainActivity.java
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/MainActivity.java
@@ -9,6 +9,6 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.game_view);
+        setContentView(R.layout.stats_view);
     }
 }
