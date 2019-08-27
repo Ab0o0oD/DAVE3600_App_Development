10678a3ff6ca333177db4d2c96a85e5be3320e48
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-27 10:52:28 +0200

    Created project for the first graded assignment

---
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/MainActivity.java
@@ -0,0 +1,14 @@
+package com.fredrikpedersen.s306631mappe1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
