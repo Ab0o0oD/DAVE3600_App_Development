30afd2b93421065d06c4108bf0bf8e6c40b47b45
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-28 18:43:52 +0200

    Implemented statistics functionality

--- s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Stats.java
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Stats.java
@@ -2,6 +2,8 @@ package com.fredrikpedersen.s306631mappe1;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.view.View;
+import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,10 +12,35 @@ public class Stats extends AppCompatActivity {
 
     public static final String TAG = "Stats";
 
+    private TextView correctAnswers;
+    private TextView wrongAnswers;
+    private int correct = Data.getCorrect();
+    private int wrong = Data.getWrong();
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.stats_activity);
+
+        initializeViews();
+        showStats();
+    }
+
+    public void deleteStats(View view) {
+        Data.setCorrect(0);
+        Data.setWrong(0);
+        correctAnswers.setText("0");
+        wrongAnswers.setText("0");
+    }
+
+    private void initializeViews() {
+        correctAnswers = findViewById(R.id.correctAnswers);
+        wrongAnswers = findViewById(R.id.wrongAnswers);
+    }
+
+    private void showStats() {
+        correctAnswers.setText(String.valueOf(correct));
+        wrongAnswers.setText(String.valueOf(wrong));
     }
 
     /* ---------- Life-Cycle Methods ------------- */
