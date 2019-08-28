8b064ce0debec329e83907340e01800dd071f78f
Fredrik Pedersen <fredrikhp@gmail.com>
2019-08-28 17:53:48 +0200

    Added an AlertDialog when the user hits the backbutton on the main menu

--- s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/MainMenu.java
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/MainMenu.java
@@ -1,8 +1,10 @@
 package com.fredrikpedersen.s306631mappe1;
 
 import androidx.annotation.NonNull;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
@@ -18,6 +20,8 @@ public class MainMenu extends AppCompatActivity {
         setContentView(R.layout.main_menu);
     }
 
+    /* ----- Button Methods ------ */
+
     public void startGame(View view) {
         Intent intent = new Intent(this, Game.class);
         startActivity(intent);
@@ -32,6 +36,23 @@ public class MainMenu extends AppCompatActivity {
         Intent intent = new Intent(this, Preferences.class);
         startActivity(intent);
     }
+
+    @Override
+    public void onBackPressed() {
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setMessage(getResources().getString(R.string.backPressed))
+                .setCancelable(false)
+                .setPositiveButton(getResources().getString(R.string.yes), new DialogInterface.OnClickListener() {
+                    @Override
+                    public void onClick(DialogInterface dialogInterface, int i) {
+                        finish();
+                    }
+                })
+                .setNegativeButton(getResources().getString(R.string.no), null)
+                .show();
+    }
+
+    /* ---------- Life-Cycle Methods ------------- */
 
     @Override
     protected void onStart() {
