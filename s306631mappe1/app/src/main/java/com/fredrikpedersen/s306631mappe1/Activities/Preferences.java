b96bd469342b67a88426383a039c0019cfb13d32
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-02 13:58:44 +0200

    Removed unnecessary feedback

--- s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Activities/Preferences.java
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Activities/Preferences.java
@@ -5,7 +5,6 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
@@ -17,27 +16,22 @@ import com.fredrikpedersen.s306631mappe1.R;
 public class Preferences extends BaseActivity {
 
     public static final String TAG = "Preferences";
-    Toast toast;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.preferences_activity);
-
-        toast = Toast.makeText(getApplicationContext(), getResources().getString(R.string.germanChosen), Toast.LENGTH_SHORT);
-        toast.show();
     }
 
     public void chooseNumberOfTasks(View view) {
         Button btn = (Button)view;
         Data.setNumberOfTasks(Integer.valueOf(btn.getText().toString()));
-        System.out.println(Data.getNumberOfTasks());
     }
 
     public void selectLanguage(View view) {
-        if (view == findViewById(R.id.germanFlag)) {
+        if (view == findViewById(R.id.germanFlag)) { //Make this if-else into a switch/case if more languages are added
             setLocale(this, LocaleManager.GERMAN);
-        } else if (view == findViewById(R.id.norwegianFlag)) { //TODO make this if-else into a switch/case if more languages are added
+        } else if (view == findViewById(R.id.norwegianFlag)) {
             setLocale(this, LocaleManager.NORWEGIAN);
         }
     }
