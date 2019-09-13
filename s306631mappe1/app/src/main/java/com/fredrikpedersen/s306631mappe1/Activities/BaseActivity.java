c368c958d9cc7cb32d785d009f271dbc48e3a3fe
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-13 09:54:33 +0200

    Revert "Removed LocaleManager in favour of a simpler solution."
    This reverts commit ea13f887d7fc10726a7eb1aab536a4f401a649f8.

--- s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Activities/BaseActivity.java
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Activities/BaseActivity.java
@@ -1,5 +1,6 @@
 package com.fredrikpedersen.s306631mappe1.Activities;
 
+import com.fredrikpedersen.s306631mappe1.LocaleManager;
 import android.content.Context;
 import android.os.Bundle;
 import androidx.appcompat.app.AppCompatActivity;
@@ -14,6 +15,11 @@ public abstract class BaseActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+    }
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(LocaleManager.setLocale(base));
     }
 
     @Override
--- s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Activities/Preferences.java
+++ s306631mappe1/app/src/main/java/com/fredrikpedersen/s306631mappe1/Activities/Preferences.java
@@ -1,15 +1,14 @@
 package com.fredrikpedersen.s306631mappe1.Activities;
 
-import android.content.res.Configuration;
-import android.content.res.Resources;
-import android.graphics.Color;
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 
-import com.fredrikpedersen.s306631mappe1.R;
+import androidx.appcompat.app.AppCompatActivity;
 
-import java.util.Locale;
+import com.fredrikpedersen.s306631mappe1.LocaleManager;
+import com.fredrikpedersen.s306631mappe1.R;
 
 public class Preferences extends BaseActivity {
 
@@ -45,21 +44,17 @@ public class Preferences extends BaseActivity {
     //Assigned to norwegianFlag and germanFlag in preferences_activity.xml
     public void selectLanguage(View view) {
         if (view == findViewById(R.id.germanFlag)) { //Make this if-else into a switch/case if more languages are added
-            setLocale("de");
-            recreate();
+            setLocale(this, LocaleManager.GERMAN);
         } else if (view == findViewById(R.id.norwegianFlag)) {
-            setLocale("nb");
-            recreate();
+            setLocale(this, LocaleManager.NORWEGIAN);
         }
     }
 
     //Sets the locale and restarts the activity
-    private void setLocale(String localeCode) {
-        Resources res = getResources();
-        Configuration cf = res.getConfiguration();
-
-        cf.setLocale(new Locale(localeCode));
-        res.updateConfiguration(cf, res.getDisplayMetrics());
+    private void setLocale(AppCompatActivity context, @LocaleManager.LocaleDef String language) {
+        LocaleManager.setNewLocale(this, language);
+        Intent intent = context.getIntent();
+        startActivity(intent.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TASK | Intent.FLAG_ACTIVITY_NEW_TASK));
     }
 
     //Initializes all the views
