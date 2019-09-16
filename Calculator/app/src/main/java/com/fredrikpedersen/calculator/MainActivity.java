3e1bafe15e4cc47a3bc908cc475d32202cc32b6a
Fredrik Pedersen <fredrikhp@gmail.com>
2019-09-16 17:42:30 +0200

    Finished Section 6: Part 80

--- Calculator/app/src/main/java/com/fredrikpedersen/calculator/MainActivity.java
+++ Calculator/app/src/main/java/com/fredrikpedersen/calculator/MainActivity.java
@@ -11,6 +11,7 @@ import android.widget.TextView;
 public class MainActivity extends AppCompatActivity {
 
     private EditText newNumber;
+    private EditText result;
     private TextView displayOperation;
 
     //Variables to hold the operands and type of calculations
@@ -45,11 +46,45 @@ public class MainActivity extends AppCompatActivity {
     }
 
     private void performOperation(String value, String operation) {
-        displayOperation.setText(operation);
+        if (operand1 == null) {
+            operand1 = Double.valueOf(value);
+        } else {
+            operand2 = Double.valueOf(value);
+        }
+
+        if (pendingOperation.equals("=")) {
+            pendingOperation = operation;
+        }
+
+        switch (pendingOperation) {
+            case "=":
+                operand1 = operand2;
+                break;
+            case "/":
+                if (operand2 == 0) {
+                    operand1 = 0.0;
+                } else {
+                    operand1 /= operand2;
+                }
+                break;
+            case "*":
+                operand1 *= operand2;
+                break;
+            case "-":
+                operand1 -= operand2;
+                break;
+            case "+":
+                operand1 += operand2;
+                break;
+        }
+
+        result.setText(operand1.toString());
+        newNumber.setText("");
     }
 
     private void initializeViews() {
         newNumber = findViewById(R.id.newNumber);
+        result = findViewById(R.id.result);
         displayOperation = findViewById(R.id.operation);
     }
 
