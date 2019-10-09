1c9f6a2ea62d29e3cd3aff03541f42bb41f48b63
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-09 20:22:00 +0200

    Completed Section 7: Part 88: Async Task

--- Top10DownloadedApp/app/src/main/java/com/fredrikpedersen/top10downloadedapp/MainActivity.java
+++ Top10DownloadedApp/app/src/main/java/com/fredrikpedersen/top10downloadedapp/MainActivity.java
@@ -2,6 +2,7 @@ package com.fredrikpedersen.top10downloadedapp;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.os.AsyncTask;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
@@ -10,5 +11,18 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    private class DownloadData extends AsyncTask<String, Void, String> {
+
+        @Override
+        protected String doInBackground(String... strings) {
+            return null;
+        }
+
+        @Override
+        protected void onPostExecute(String s) {
+            super.onPostExecute(s);
+        }
     }
 }
