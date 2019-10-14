5ba61309f666bb88d76e6721c150a8a4b723db59
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-14 20:43:55 +0200

    Updated models for use in database

---
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/database/models/Booking.java
@@ -0,0 +1,78 @@
+package com.fredrikpedersen.eatingwithfriends_gradedassignment.database.models;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Entity(tableName = "booking_table")
+public class Booking {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String restaurantName;
+    private String address;
+    private String date;
+    private String time;
+    private List<Friend> friends;
+
+    public Booking(String restaurantName, String address, String date, String time, @Nullable List<Friend> friends) {
+        this.restaurantName = restaurantName;
+        this.address = address;
+        this.date = date;
+        this.time = time;
+        this.friends = friends;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getRestaurantName() {
+        return restaurantName;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public String getTime() {
+        return time;
+    }
+
+    public List<Friend> getFriends() {
+        if (friends != null) {
+            return friends;
+        } else {
+            return new ArrayList<>();
+        }
+    }
+
+    @Override
+    @NonNull
+    public String toString() {
+
+        StringBuilder sb = new StringBuilder();
+        if (friends != null && friends.size() > 0) {
+            for (Friend friend : friends) {
+                sb.append(friend.getName()).append(" ");
+            }
+        }
+
+        return "RestaurantName= " + restaurantName + '\n' +
+                "Address= " + address + '\n' +
+                "BookingTime= " + date + " " + time + '\n' +
+                "Friends= " + sb.toString();
+    }
+}
---
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/database/models/Friend.java
@@ -0,0 +1,42 @@
+package com.fredrikpedersen.eatingwithfriends_gradedassignment.database.models;
+
+import androidx.annotation.NonNull;
+import androidx.room.Entity;
+import androidx.room.PrimaryKey;
+
+@Entity(tableName = "friend_table")
+public class Friend {
+
+    @PrimaryKey(autoGenerate = true)
+    private int id;
+    private String name;
+    private String phoneNumber;
+
+    public Friend(String name, String phoneNumber) {
+        this.name = name;
+        this.phoneNumber = phoneNumber;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getPhoneNumber() {
+        return phoneNumber;
+    }
+
+    @NonNull
+    @Override
+    public String toString() {
+        return "Name= " + name + '\n' +
+                "PhoneNumber='" + phoneNumber + '\n';
+    }
+}
