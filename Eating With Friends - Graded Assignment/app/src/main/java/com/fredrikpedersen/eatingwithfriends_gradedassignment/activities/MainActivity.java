5796e169a608b03d9e3206030956a1b13a747211
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-15 14:42:21 +0200

    Tested that Booking Items from Database can be shown in a View

--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/activities/MainActivity.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/activities/MainActivity.java
@@ -3,16 +3,27 @@ package com.fredrikpedersen.eatingwithfriends_gradedassignment.activities;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.R;
+import com.fredrikpedersen.eatingwithfriends_gradedassignment.database.models.Booking;
+import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings.BookingAdapter;
+import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings.BookingViewModel;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings.BookingsFragment;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.friends.FriendsFragment;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.notifications.NotificationsFragment;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -23,6 +34,8 @@ public class MainActivity extends AppCompatActivity {
     private final FragmentManager fragmentManager = getSupportFragmentManager();
     private Fragment activeFragment = bookingFragment;
 
+    private BookingViewModel bookingViewModel;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -30,6 +43,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
+
+        RecyclerView recyclerView = findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(this));
+        recyclerView.setHasFixedSize(true);
+
+        BookingAdapter bookingAdapter = new BookingAdapter();
+        recyclerView.setAdapter(bookingAdapter);
+
+        bookingViewModel = ViewModelProviders.of(this).get(BookingViewModel.class);
+        bookingViewModel.getAllBookings().observe(this, bookingAdapter::submitList);
+
+
         //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
         /* fragmentManager.beginTransaction().add(R.id.main_container, notificationsFragment, "notifications").hide(notificationsFragment).commit();
