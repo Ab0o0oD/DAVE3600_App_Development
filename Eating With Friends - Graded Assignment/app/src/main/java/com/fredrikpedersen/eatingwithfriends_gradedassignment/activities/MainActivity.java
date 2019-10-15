d4b600405fb2c99d6f7918807c8dbd67ca649eb8
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-15 15:57:49 +0200

    Moved RecyclerView for Bookings from MainActivity to BookingsFragment

--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/activities/MainActivity.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/activities/MainActivity.java
@@ -3,24 +3,12 @@ package com.fredrikpedersen.eatingwithfriends_gradedassignment.activities;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.widget.TextView;
-import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 import androidx.fragment.app.FragmentManager;
-import androidx.lifecycle.LiveData;
-import androidx.lifecycle.ViewModelProvider;
-import androidx.lifecycle.ViewModelProviders;
-import androidx.recyclerview.widget.ItemTouchHelper;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.R;
-import com.fredrikpedersen.eatingwithfriends_gradedassignment.database.models.Booking;
-import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings.BookingAdapter;
-import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings.BookingViewModel;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings.BookingsFragment;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.friends.FriendsFragment;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.notifications.NotificationsFragment;
@@ -35,8 +23,6 @@ public class MainActivity extends AppCompatActivity {
     private final FragmentManager fragmentManager = getSupportFragmentManager();
     private Fragment activeFragment = bookingFragment;
 
-    private BookingViewModel bookingViewModel;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -44,39 +30,14 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
 
-        RecyclerView recyclerView = findViewById(R.id.recycler_view);
-        recyclerView.setLayoutManager(new LinearLayoutManager(this));
-        recyclerView.setHasFixedSize(true);
-
-        BookingAdapter bookingAdapter = new BookingAdapter();
-        recyclerView.setAdapter(bookingAdapter);
-
-        bookingViewModel = ViewModelProviders.of(this).get(BookingViewModel.class);
-        bookingViewModel.getAllBookings().observe(this, bookingAdapter::submitList);
-
-        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
-            @Override
-            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
-                return false;
-            }
-
-            @Override
-            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
-                bookingViewModel.delete(bookingAdapter.getBookingAt(viewHolder.getAdapterPosition()));
-                Toast.makeText(MainActivity.this, "Booking Deleted", Toast.LENGTH_SHORT).show();
-            }
-        }).attachToRecyclerView(recyclerView);
-
-
-        //navigation.setOnNavigationItemSelectedListener(mOnNavigationItemSelectedListener);
-
-        /* fragmentManager.beginTransaction().add(R.id.main_container, notificationsFragment, "notifications").hide(notificationsFragment).commit();
+        fragmentManager.beginTransaction().add(R.id.main_container, notificationsFragment, "notifications").hide(notificationsFragment).commit();
         fragmentManager.beginTransaction().add(R.id.main_container, friendsFragment, "friends").hide(friendsFragment).commit();
-        fragmentManager.beginTransaction().add(R.id.main_container, bookingFragment, "booking").commit(); */
+        fragmentManager.beginTransaction().add(R.id.main_container, bookingFragment, "booking").commit();
     }
 
-   /* private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
+    private BottomNavigationView.OnNavigationItemSelectedListener mOnNavigationItemSelectedListener = item -> {
         switch (item.getItemId()) {
             case R.id.navigation_bookings:
                 fragmentManager.beginTransaction().hide(activeFragment).show(bookingFragment).commit();
@@ -94,7 +55,7 @@ public class MainActivity extends AppCompatActivity {
                 return true;
         }
         return false;
-    }; */
+    };
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/bookings/BookingsFragment.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/bookings/BookingsFragment.java
@@ -1,33 +1,28 @@
 package com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.bookings;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
-import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProviders;
+import androidx.recyclerview.widget.ItemTouchHelper;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.R;
-import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.pickers.DatePickerFragment;
-import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.pickers.OnPickerValueSelectedListener;
-import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.pickers.TimePickerFragment;
+import com.fredrikpedersen.eatingwithfriends_gradedassignment.database.models.Booking;
+import com.google.android.material.floatingactionbutton.FloatingActionButton;
 
-public class BookingsFragment extends Fragment implements OnPickerValueSelectedListener {
+public class BookingsFragment extends Fragment {
 
     private static final String TAG = "BookingsFragment";
-    private final String[] FRAGMENTS = {"timepicker", "datepicker"};
-    private TextView dateTimePrint;
-    private Button printBtn;
-    private Button timeBtn;
-    private Button dateBtn;
-    private String activePicker;
-
-    private String time = "";
-    private String date = "";
+    private BookingViewModel bookingViewModel;
 
     public BookingsFragment() { }
 
@@ -36,60 +31,49 @@ public class BookingsFragment extends Fragment implements OnPickerValueSelectedL
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_bookings, container, false);
 
-        initializeViews(view);
-        setOnClicks();
+        FloatingActionButton addButton = view.findViewById(R.id.button_add_booking);
+        addButton.setOnClickListener(v -> {
+            bookingViewModel.insert(new Booking("Mi Case", "Seven 11", "29 October 2019", "13:37", null));
+            Toast.makeText(getActivity(), "New booking Created", Toast.LENGTH_SHORT);
+        });
+
+        RecyclerView recyclerView = view.findViewById(R.id.recycler_view);
+        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity()));
+        recyclerView.setHasFixedSize(true);
+
+        BookingAdapter bookingAdapter = new BookingAdapter();
+        recyclerView.setAdapter(bookingAdapter);
+
+        bookingViewModel = ViewModelProviders.of(this).get(BookingViewModel.class);
+        bookingViewModel.getAllBookings().observe(this, bookingAdapter::submitList);
+        Log.d(TAG, "onCreateView: PRINTING GETALLBOOKINGS" + bookingViewModel.getAllBookings().toString());
+
+        new ItemTouchHelper(new ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT | ItemTouchHelper.RIGHT) {
+            @Override
+            public boolean onMove(@NonNull RecyclerView recyclerView, @NonNull RecyclerView.ViewHolder viewHolder, @NonNull RecyclerView.ViewHolder target) {
+                return false;
+            }
+
+            @Override
+            public void onSwiped(@NonNull RecyclerView.ViewHolder viewHolder, int direction) {
+                bookingViewModel.delete(bookingAdapter.getBookingAt(viewHolder.getAdapterPosition()));
+                Toast.makeText(getActivity(), "Booking Deleted", Toast.LENGTH_SHORT).show();
+            }
+        }).attachToRecyclerView(recyclerView);
 
         return view;
     }
 
 
-    private void appendText() {
-        if (date.equals("") || time.equals("")) {
-            dateTimePrint.setText("Date and time now set");
-        } else {
-            dateTimePrint.setText(date + " " + time);
-        }
-    }
 
     /* ----- Initializations ----- */
 
     private void initializeViews(View view) {
-        dateTimePrint = view.findViewById(R.id.dateTimePrint);
-        printBtn = view.findViewById(R.id.printBtn);
-        timeBtn = view.findViewById(R.id.pickTimeBtn);
-        dateBtn = view.findViewById(R.id.pickDateBtn);
     }
 
     private void setOnClicks() {
-        printBtn.setOnClickListener(v -> appendText());
-        timeBtn.setOnClickListener(v -> showTimePickerDialog());
-        dateBtn.setOnClickListener(v -> showDatePickerDialog());
     }
 
-    /* ----- Date and Time Pickers ----- */
-
-    private void showTimePickerDialog() {
-        TimePickerFragment newTimePicker = new TimePickerFragment();
-        newTimePicker.setOnTimeSelectedListener(this);
-        newTimePicker.show(getChildFragmentManager(), "timePicker");
-        activePicker = FRAGMENTS[0];
-    }
-
-    private void showDatePickerDialog() {
-        DatePickerFragment newDatePicker = new DatePickerFragment();
-        newDatePicker.setOnDateSelectedListener(this);
-        newDatePicker.show(getChildFragmentManager(), "datePicker");
-        activePicker = FRAGMENTS[1];
-    }
-
-    @Override
-    public void onValueSelected(String data) {
-        if (activePicker.equals(FRAGMENTS[0]))
-            time = data;
-        else if (activePicker.equals(FRAGMENTS[1]))
-            date = data;
-
-    }
 
 
 
