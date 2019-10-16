f36f82ddc62f840131ed177f37a0fb9860771712
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-16 20:19:25 +0200

    Set TimePickerFragment to use a 24-hour format

--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/pickers/TimePickerFragment.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/pickers/TimePickerFragment.java
@@ -28,7 +28,7 @@ public class TimePickerFragment extends DialogFragment implements TimePickerDial
         int hour = c.get(Calendar.HOUR_OF_DAY);
         int minute = c.get(Calendar.MINUTE);
 
-        return new TimePickerDialog(getActivity(), this, hour, minute, DateFormat.is24HourFormat(getActivity()));
+        return new TimePickerDialog(getActivity(), this, hour, minute, true);
     }
 
     public void onTimeSet(TimePicker view, int hourOfDay, int minute) {
