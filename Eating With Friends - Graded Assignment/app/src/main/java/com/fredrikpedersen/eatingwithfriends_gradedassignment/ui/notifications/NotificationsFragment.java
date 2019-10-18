b6dc5460fab84d874c1c13b29d60e3666f712520
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-18 14:52:33 +0200

    Implemented simple SMS sender functionality

--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/notifications/NotificationsFragment.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/notifications/NotificationsFragment.java
@@ -1,26 +1,48 @@
 package com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.notifications;
 
 import android.os.Bundle;
+import android.telephony.SmsManager;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
+import android.widget.Button;
+import android.widget.Toast;
 
-import androidx.annotation.Nullable;
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.lifecycle.Observer;
-import androidx.lifecycle.ViewModelProviders;
 
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.R;
 
 public class NotificationsFragment extends Fragment {
+
+    private Button buttonSendSms;
 
     public NotificationsFragment() {}
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_notifications, container, false);
 
+        initializeViews(view);
+        setOnlicks();
+
         return view;
+    }
+
+    private void initializeViews(View view) {
+        buttonSendSms = view.findViewById(R.id.button_send_sms);
+    }
+
+    private void setOnlicks() {
+        buttonSendSms.setOnClickListener(v -> sendSms());
+    }
+
+    private void sendSms() {
+        try {
+            SmsManager smsManager = SmsManager.getDefault();
+            smsManager.sendTextMessage("+15555215556", null, "Hello There", null, null);
+            Toast.makeText(getActivity(), "SMS Send Successfully", Toast.LENGTH_SHORT).show();
+        } catch (Exception e) {
+            Toast.makeText(getActivity(), "Sending SMS Failed", Toast.LENGTH_SHORT).show();
+        }
     }
 }
