81c9df712eadc7f553cfe8f354db32205cea563e
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-18 12:02:35 +0200

    Created Testproject for sending SMS

---
+++ LectureAssignments/SMSTesting/app/src/main/java/com/fredrikpedersen/smstesting/MainActivity.java
@@ -0,0 +1,39 @@
+package com.fredrikpedersen.smstesting;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.telephony.SmsManager;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+public class MainActivity extends AppCompatActivity {
+    private EditText txtMobile;
+    private EditText txtMessage;
+    private Button btnSms;
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        txtMobile = findViewById(R.id.mblTxt);
+        txtMessage = findViewById(R.id.msgTxt);
+        btnSms = findViewById(R.id.btnSend);
+
+        btnSms.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                try{
+                    SmsManager smgr = SmsManager.getDefault();
+                    smgr.sendTextMessage(txtMobile.getText().toString(),null,txtMessage.getText().toString(),null,null);
+                    Toast.makeText(MainActivity.this, "SMS Sent Successfully", Toast.LENGTH_SHORT).show();
+                }
+                catch (Exception e){
+                    Toast.makeText(MainActivity.this, "SMS Failed to Send, Please try again", Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+    }
+}
