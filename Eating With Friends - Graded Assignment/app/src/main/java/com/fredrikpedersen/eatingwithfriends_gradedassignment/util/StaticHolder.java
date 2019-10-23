a6e01e23587759cfc5056c615a20d1a432135c08
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-23 18:25:17 +0200

    Added more values

--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/util/StaticHolder.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/util/StaticHolder.java
@@ -2,6 +2,7 @@ package com.fredrikpedersen.eatingwithfriends_gradedassignment.util;
 
 public class StaticHolder {
 
+    /* ----- EXTRAS ----- */
     public static final String EXTRA_ID = "com.fredrikpedersen.eatingwithfriends_gradedassignment.activities.EXTRA_ID";
     public static final String EXTRA_RESTAURANT_NAME = "com.fredrikpedersen.eatingwithfriends_gradedassignment.activities.EXTRA_RESTAURANT_NAME";
     public static final String EXTRA_RESTAURANT_ADDRESS = "com.fredrikpedersen.eatingwithfriends_gradedassignment.activities.EXTRA_RESTAURANT_ADDRESS";
@@ -14,7 +15,7 @@ public class StaticHolder {
     public static final String EXTRA_FIRST_NAME = "com.fredrikpedersen.eatingwithfriends_gradedassignment.activities.EXTRA_FIRST_NAME";
     public static final String EXTRA_LAST_NAME = "com.fredrikpedersen.eatingwithfriends_gradedassignment.activities.EXTRA_LAST_NAME";
 
-
+    /* ------ REQUESTS ----- */
     public static final int ADD_BOOKING_REQUEST = 1;
     public static final int EDIT_BOOKING_REQUEST = 2;
     public static final int ADD_FRIEND_REQUEST = 3;
@@ -22,6 +23,13 @@ public class StaticHolder {
     public static final int ADD_RESTAURANT_REQUEST = 5;
     public static final int EDIT_RESTAURANT_REQUEST = 6;
 
+    /* ----- PERMISSION CODES ----- */
     public static final int SMS_PERMISSION_CODE = 1;
+
+    /* ----- CONTENT STRINGS ----- */
+    public static final String PREFERENCE = "Preference";
+    public static final String SMS_FUNCTIONALITY_PREF = "SMS Functionality Pref";
+    public static final String USE_DEFAULT_MESSAGE_PREF = "Default Message Pref";
+    public static final String CUSTOM_MESSAGE_PREF = "Custom Message Pref";
 
 }
