0d9b30a8603195d1c958aeb4a946a22335365374
Fredrik Pedersen <fredrikhp@gmail.com>
2019-10-23 19:45:21 +0200

    Implemented timepicker in Settings

--- Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/settings/SettingsFragment.java
+++ Eating With Friends - Graded Assignment/app/src/main/java/com/fredrikpedersen/eatingwithfriends_gradedassignment/ui/settings/SettingsFragment.java
@@ -8,6 +8,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.TextView;
 import android.widget.Toast;
 import android.widget.ToggleButton;
 
@@ -17,28 +18,32 @@ import androidx.core.app.ActivityCompat;
 import androidx.fragment.app.Fragment;
 
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.R;
+import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.pickers.OnPickerValueSelectedListener;
+import com.fredrikpedersen.eatingwithfriends_gradedassignment.ui.pickers.TimePickerFragment;
 import com.fredrikpedersen.eatingwithfriends_gradedassignment.util.StaticHolder;
 
 import java.util.Objects;
 
-public class SettingsFragment extends Fragment {
+public class SettingsFragment extends Fragment implements OnPickerValueSelectedListener {
 
     private static final String TAG = "SettingsFragment";
 
     private ToggleButton buttonToggleSms;
     private Button buttonSaveMessage;
     private Button buttonUseDefault;
+    private TextView textViewTimePicker;
     private EditText editTextMessage;
 
     private boolean smsFunctionality;
     private boolean useDefaultMessage;
-    private String customMessage = "";
+    private String smsTime;
 
     public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.fragment_settings, container, false);
 
         smsFunctionality = smsPermission();
         useDefaultMessage = Objects.requireNonNull(getActivity()).getSharedPreferences(StaticHolder.PREFERENCE, Context.MODE_PRIVATE).getBoolean(StaticHolder.USE_DEFAULT_MESSAGE_PREF, true);
+        smsTime = Objects.requireNonNull(getActivity()).getSharedPreferences(StaticHolder.PREFERENCE, Context.MODE_PRIVATE).getString(StaticHolder.SMS_TIMING_PREF, "12:00");
 
         initializeViews(view);
         setOnlicks();
@@ -46,12 +51,12 @@ public class SettingsFragment extends Fragment {
         return view;
     }
 
-    private void settMessageCustom() {
+    private void setMessageCustom() {
         if (editTextMessage.getText().toString().equals("")) {
             Toast.makeText(getActivity(), "The message box is empty", Toast.LENGTH_SHORT).show();
         } else {
             useDefaultMessage = false;
-            customMessage = editTextMessage.getText().toString();
+            String customMessage = editTextMessage.getText().toString();
 
             Objects.requireNonNull(getActivity()).getSharedPreferences(StaticHolder.PREFERENCE, Context.MODE_PRIVATE)
                     .edit()
@@ -103,6 +108,23 @@ public class SettingsFragment extends Fragment {
         return !ActivityCompat.shouldShowRequestPermissionRationale(Objects.requireNonNull(getActivity()), Manifest.permission.SEND_SMS);
     }
 
+    private void showTimePickerDialog() {
+        TimePickerFragment timePicker = new TimePickerFragment();
+        timePicker.setOnTimeSelectedListener(this);
+        timePicker.show(Objects.requireNonNull(getActivity()).getSupportFragmentManager(), "timePicker");
+    }
+
+    @Override
+    public void onValueSelected(String data) {
+        smsTime = data;
+        textViewTimePicker.setText(data);
+
+        Objects.requireNonNull(getActivity()).getSharedPreferences(StaticHolder.PREFERENCE, Context.MODE_PRIVATE)
+                .edit()
+                .putString(StaticHolder.SMS_TIMING_PREF, smsTime)
+                .apply();
+    }
+
     private void initializeViews(View view) {
         buttonToggleSms = view.findViewById(R.id.toggle_button_sms);
         buttonToggleSms.setChecked(smsFunctionality);
@@ -110,11 +132,15 @@ public class SettingsFragment extends Fragment {
         buttonSaveMessage = view.findViewById(R.id.button_save_message);
         buttonUseDefault = view.findViewById(R.id.button_use_default);
         editTextMessage = view.findViewById(R.id.edit_text_write_message);
+        textViewTimePicker = view.findViewById(R.id.text_view_time_picker_sms);
+        textViewTimePicker.setText(smsTime);
     }
 
     private void setOnlicks() {
         buttonToggleSms.setOnClickListener(v -> toggleSmsOn());
         buttonUseDefault.setOnClickListener(v -> setMessageDefault());
-        buttonSaveMessage.setOnClickListener(v -> settMessageCustom());
+        buttonSaveMessage.setOnClickListener(v -> setMessageCustom());
+        textViewTimePicker.setOnClickListener(v -> showTimePickerDialog());
     }
+
 }
