7d1758358ec461d49d9fc41b1233c32534bba492
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-21 16:08:25 +0100

    Added Building View Model

---
+++ Room4You/app/src/main/java/com/s306631/room4you/viewModels/BuildingViewModel.java
@@ -0,0 +1,27 @@
+package com.s306631.room4you.viewModels;
+
+import android.app.Application;
+
+import androidx.annotation.NonNull;
+import androidx.lifecycle.AndroidViewModel;
+
+import com.s306631.room4you.models.Building;
+import com.s306631.room4you.repository.BuildingRepository;
+
+import java.util.List;
+
+public class BuildingViewModel extends AndroidViewModel {
+
+    private static final String TAG = "BuildingViewModel";
+
+    private BuildingRepository buildingRepository;
+
+    public BuildingViewModel(@NonNull Application application) {
+        super(application);
+        buildingRepository = new BuildingRepository();
+    }
+
+    public List<Building> getAllBuildingsAsList() {
+        return buildingRepository.getBuildingsFromWebService();
+    }
+}
