4ff85570881232a7d172e75e6cbc5cbb3e30c0a1
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-22 14:01:36 +0100

    Added getters and setters for all attributes

--- Room4You/app/src/main/java/com/s306631/room4you/models/Room.java
+++ Room4You/app/src/main/java/com/s306631/room4you/models/Room.java
@@ -23,7 +23,51 @@ public class Room {
         this.coordinates = CoordinatesParser.coordinatesFromString(coordinates);
     }
 
+    public static String getTAG() {
+        return TAG;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public int getBuildingId() {
+        return buildingId;
+    }
+
+    public void setBuildingId(int buildingId) {
+        this.buildingId = buildingId;
+    }
+
+    public String getRoomName() {
+        return roomName;
+    }
+
+    public void setRoomName(String roomName) {
+        this.roomName = roomName;
+    }
+
+    public int getFloor() {
+        return floor;
+    }
+
+    public void setFloor(int floor) {
+        this.floor = floor;
+    }
+
+    public LatLng getCoordinates() {
+        return coordinates;
+    }
+
+    public void setCoordinates(LatLng coordinates) {
+        this.coordinates = coordinates;
+    }
+
     public String toString() {
-        return roomId + " " + buildingId + " " + roomName + " " + floor + " " + coordinates.toString();
+        return roomName;
     }
 }
