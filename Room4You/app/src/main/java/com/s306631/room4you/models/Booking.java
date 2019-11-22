3c6d0a7ef625f9bf2bb0dd1826f0ba73f37db307
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-22 19:13:42 +0100

    Made changes to Booking Model according to changes in the database

--- Room4You/app/src/main/java/com/s306631/room4you/models/Booking.java
+++ Room4You/app/src/main/java/com/s306631/room4you/models/Booking.java
@@ -4,13 +4,55 @@ public class Booking {
 
     private int bookingId;
     private int roomId;
+    private String bookerName;
     private String fromTime;
     private String toTime;
 
-    public Booking(int bookingId, int roomId, String fromTime, String toTime) {
+    public Booking(int bookingId, int roomId, String bookerName, String fromTime, String toTime) {
         this.bookingId = bookingId;
         this.roomId = roomId;
+        this.bookerName = bookerName;
         this.fromTime = fromTime;
+        this.toTime = toTime;
+    }
+
+    public int getBookingId() {
+        return bookingId;
+    }
+
+    public void setBookingId(int bookingId) {
+        this.bookingId = bookingId;
+    }
+
+    public int getRoomId() {
+        return roomId;
+    }
+
+    public void setRoomId(int roomId) {
+        this.roomId = roomId;
+    }
+
+    public String getBookerName() {
+        return bookerName;
+    }
+
+    public void setBookerName(String bookerName) {
+        this.bookerName = bookerName;
+    }
+
+    public String getFromTime() {
+        return fromTime;
+    }
+
+    public void setFromTime(String fromTime) {
+        this.fromTime = fromTime;
+    }
+
+    public String getToTime() {
+        return toTime;
+    }
+
+    public void setToTime(String toTime) {
         this.toTime = toTime;
     }
 
