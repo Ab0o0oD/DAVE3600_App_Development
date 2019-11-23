7ad5b31957615d4e8d0a7b5d9c167d07848c2f05
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-23 17:23:37 +0100

    Made confirm and cancel into Buttons instead of TextViews

--- Room4You/app/src/main/java/com/s306631/room4you/ui/CustomDialog.java
+++ Room4You/app/src/main/java/com/s306631/room4you/ui/CustomDialog.java
@@ -3,6 +3,7 @@ package com.s306631.room4you.ui;
 import android.app.Activity;
 import android.app.Dialog;
 import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import com.s306631.room4you.R;
@@ -10,7 +11,8 @@ import com.s306631.room4you.R;
 public class CustomDialog extends Dialog implements View.OnClickListener {
     private static final String TAG = "CustomDialog";
 
-    private TextView content, confirm, cancel;
+    private TextView content;
+    private Button confirm, cancel;
     private OnDialogOptionSelectedListener callback;
 
     public void setOnDialogOptionSelectedListener(OnDialogOptionSelectedListener callback) {
@@ -28,9 +30,9 @@ public class CustomDialog extends Dialog implements View.OnClickListener {
     private void initializeViews() {
         content = findViewById(R.id.text_view_dialog_message);
 
-        confirm = findViewById(R.id.text_view_dialog_confirm);
+        confirm = findViewById(R.id.button_dialog_confirm);
         confirm.setOnClickListener(this);
-        cancel = findViewById(R.id.text_view_dialog_dissmiss);
+        cancel = findViewById(R.id.button_dialog_dissmiss);
         cancel.setOnClickListener(this);
     }
 
