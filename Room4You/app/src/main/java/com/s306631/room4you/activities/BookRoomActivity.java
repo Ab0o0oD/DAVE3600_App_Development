0da9c15e22753d7c855075cc9652872787a48f6d
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-24 17:31:30 +0100

    Added methods for refreshing the activity's views after selecting a date and registering a new booking. Added functionality for letting the user choose what date they want to book a room.

--- Room4You/app/src/main/java/com/s306631/room4you/activities/BookRoomActivity.java
+++ Room4You/app/src/main/java/com/s306631/room4you/activities/BookRoomActivity.java
@@ -3,13 +3,14 @@ package com.s306631.room4you.activities;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.view.WindowManager;
 import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Spinner;
 import android.widget.TextView;
-import android.widget.Toast;
 
 import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
@@ -18,13 +19,11 @@ import androidx.lifecycle.ViewModelProviders;
 import com.s306631.room4you.R;
 import com.s306631.room4you.models.Booking;
 import com.s306631.room4you.models.Room;
+import com.s306631.room4you.ui.pickers.DatePickerFragment;
+import com.s306631.room4you.ui.pickers.OnPickerValueSelectedListener;
+import com.s306631.room4you.util.DateFormater;
 import com.s306631.room4you.viewModels.BookingViewModel;
 
-import java.io.IOException;
-import java.net.HttpURLConnection;
-import java.net.MalformedURLException;
-import java.net.ProtocolException;
-import java.net.URL;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Arrays;
@@ -32,23 +31,22 @@ import java.util.Calendar;
 import java.util.Date;
 import java.util.List;
 
-public class BookRoomActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener {
+public class BookRoomActivity extends AppCompatActivity implements AdapterView.OnItemSelectedListener, OnPickerValueSelectedListener {
 
     public static Room selectedRoom = null;
     private static final String TAG = "BookRoomActivity";
 
-    private TextView textViewHeadline, textViewAvailableTimes, textViewRegisteredHeadline, textViewTakenTimes, textViewBookerNames;
+    private TextView textViewHeadline, textViewRegisteredHeadline, textViewTakenTimes, textViewBookerNames, textViewDate;
+    private ImageView imageViewSelectDate;
     private Spinner spinnerAvailableSpinner;
     private EditText editTextBookerName;
     private Button buttonRegisterBooking;
 
     private BookingViewModel bookingViewModel;
 
-    private List<Booking> bookingsFromWebService;
-    private List<Booking> takenBookingTimes;
+    private List<Booking> bookingsFromWebService, takenBookingTimes;
     private List<String> availableBookingTimes;
-    private String selectedTime;
-    private String currentDate;
+    private String selectedTime, currentDate, selectedDate;
 
     @Override
     protected void onCreate(@Nullable Bundle savedInstanceState) {
@@ -57,38 +55,20 @@ public class BookRoomActivity extends AppCompatActivity implements AdapterView.O
 
         bookingViewModel = ViewModelProviders.of(this).get(BookingViewModel.class);
         bookingsFromWebService = bookingViewModel.getAllBookingsAsList();
-        takenBookingTimes = new ArrayList<>();
 
         Date date = Calendar.getInstance().getTime();
         SimpleDateFormat formater = new SimpleDateFormat("dd. MMM yyyy");
-        currentDate = formater.format(date);
+        selectedDate = currentDate = formater.format(date);
 
-        //Create a list of bookings belonging to the passed in Room
-        for (Booking booking : bookingsFromWebService) {
-            if (booking.getRoomId() == selectedRoom.getRoomId() && booking.getDate().equals(currentDate)) {
-                takenBookingTimes.add(booking);
-            }
-        }
 
-        String[] possibleBookingTimes = getResources().getStringArray(R.array.possible_booking_times);
-        availableBookingTimes = new ArrayList<>(Arrays.asList(possibleBookingTimes));
-
-        //Find out what booking are taken
-        for (String possibleTime : possibleBookingTimes) {
-            for (Booking takenBooking : takenBookingTimes) {
-                if (possibleTime.equals(takenBooking.getFromTime() + " - " + takenBooking.getToTime())) {
-                    availableBookingTimes.remove(possibleTime);
-                }
-            }
-        }
-
+        fillTakenBookingTimes();
+        fillAvailableBookingTimes();
         initializeViews();
         fillAvailableSpinner();
 
-        for (Booking booking : takenBookingTimes) {
-            textViewTakenTimes.append(booking.getFromTime() + " - " + booking.getToTime() + "\n");
-            textViewBookerNames.append(booking.getBookerName() + "\n");
-        }
+        //Can't find a way to prevent editTextBookerName from gaining focus when the activity loads.
+        //This at least prevents the on-screen keyboard from popping up automatically.
+        this.getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
     }
 
     private void registerBooking() {
@@ -102,13 +82,22 @@ public class BookRoomActivity extends AppCompatActivity implements AdapterView.O
         String[] timeSplit = selectedTime.split(" - ");
         String fromTime = timeSplit[0];
         String toTime = timeSplit[1];
-        Booking booking = new Booking(selectedRoom.getRoomId(), bookerName, fromTime, toTime, currentDate);
+        Booking booking = new Booking(selectedRoom.getRoomId(), bookerName, fromTime, toTime, selectedDate);
         bookingViewModel.postBooking(this, booking);
     }
 
-    //TODO Make this method refresh the activity
-    public void refresh() {
-        Toast.makeText(this, "refreshed", Toast.LENGTH_SHORT).show();
+    private void showDateSelector() {
+        DatePickerFragment datePicker = new DatePickerFragment();
+        datePicker.setOnDateSelectedListener(this);
+        datePicker.show(getSupportFragmentManager(), "datePicker");
+    }
+
+    @Override
+    public void onValueSelected(String data) {
+        String date = DateFormater.formatDateText(data);
+        textViewDate.setText(date);
+        selectedDate = date;
+        refreshAfterDateSelection();
     }
 
 
@@ -128,17 +117,51 @@ public class BookRoomActivity extends AppCompatActivity implements AdapterView.O
 
     }
 
+    private void fillTakenBookingTimes() {
+        takenBookingTimes = new ArrayList<>();
+        for (Booking booking : bookingsFromWebService) {
+            if (booking.getRoomId() == selectedRoom.getRoomId() && booking.getDate().equals(selectedDate)) {
+                takenBookingTimes.add(booking);
+            }
+        }
+    }
+
+    private void fillAvailableBookingTimes() {
+        String[] possibleBookingTimes = getResources().getStringArray(R.array.possible_booking_times);
+        availableBookingTimes = new ArrayList<>(Arrays.asList(possibleBookingTimes));
+
+        //Find out what booking are taken
+        for (String possibleTime : possibleBookingTimes) {
+            for (Booking takenBooking : takenBookingTimes) {
+                if (possibleTime.equals(takenBooking.getFromTime() + " - " + takenBooking.getToTime())) {
+                    availableBookingTimes.remove(possibleTime);
+                }
+            }
+        }
+    }
+
     private void initializeViews() {
         textViewHeadline = findViewById(R.id.text_view_bookings_headline);
         textViewHeadline.setText(selectedRoom.getRoomName());
-
-        textViewAvailableTimes = findViewById(R.id.text_view_available_times);
 
         textViewTakenTimes = findViewById(R.id.text_view_taken_times);
         textViewTakenTimes.setText("");
 
         textViewBookerNames = findViewById(R.id.text_view_booker_name);
         textViewBookerNames.setText("");
+
+
+        for (Booking booking : takenBookingTimes) {
+            textViewTakenTimes.append(booking.getFromTime() + " - " + booking.getToTime() + "\n");
+            textViewBookerNames.append(booking.getBookerName() + "\n");
+        }
+
+        textViewDate = findViewById(R.id.text_view_date);
+        textViewDate.setOnClickListener(v -> showDateSelector());
+        textViewDate.setText(selectedDate);
+
+        imageViewSelectDate = findViewById(R.id.image_view_select_date);
+        imageViewSelectDate.setOnClickListener(v -> showDateSelector());
 
         spinnerAvailableSpinner = findViewById(R.id.spinner_available_times);
         spinnerAvailableSpinner.setOnItemSelectedListener(this);
@@ -149,7 +172,34 @@ public class BookRoomActivity extends AppCompatActivity implements AdapterView.O
         editTextBookerName = findViewById(R.id.edit_text_booker_name);
 
         textViewRegisteredHeadline = findViewById(R.id.text_view_registered_bookings_headline);
-        textViewRegisteredHeadline.append(":\n" + currentDate);
+        textViewRegisteredHeadline.setText(getResources().getString(R.string.registered_bookings) + ":\n" + selectedDate);
+    }
+
+    private void refreshAfterDateSelection() {
+        Log.d(TAG, "refreshAfterDateSelection: REFRESHING PAGE FOR " + selectedDate);
+        textViewTakenTimes.setText("");
+        textViewBookerNames.setText("");
+        fillTakenBookingTimes();
+        for(Booking booking : takenBookingTimes) {
+            Log.d(TAG, "refreshAfterDateSelection: BOOKINGS ON " + selectedDate + " :" + booking.toString());
+        }
+
+        fillAvailableBookingTimes();
+        fillAvailableSpinner();
+        textViewRegisteredHeadline.setText(getResources().getString(R.string.registered_bookings) + ":\n" + selectedDate);
+
+        for (Booking booking : takenBookingTimes) {
+            textViewTakenTimes.append(booking.getFromTime() + " - " + booking.getToTime() + "\n");
+            textViewBookerNames.append(booking.getBookerName() + "\n");
+        }
+    }
+
+    public void refresh() {
+        bookingsFromWebService = bookingViewModel.getAllBookingsAsList();
+        fillTakenBookingTimes();
+        fillAvailableBookingTimes();
+        initializeViews();
+        fillAvailableSpinner();
     }
 
     @Override
