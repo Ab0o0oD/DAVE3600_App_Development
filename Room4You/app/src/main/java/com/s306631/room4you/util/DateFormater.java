79bcd9e25c7f2096179bba5badc689be86cd03cc
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-24 17:28:39 +0100

    Added DateFormater Util class

---
+++ Room4You/app/src/main/java/com/s306631/room4you/util/DateFormater.java
@@ -0,0 +1,62 @@
+package com.s306631.room4you.util;
+
+public class DateFormater {
+
+    private DateFormater() {
+    }
+
+    public static String formatDateText(String date) {
+        String[] dateSplit = date.split("-");
+        return formatDayNumber(Integer.parseInt(dateSplit[0])) + ". " + formatMonthText(Integer.parseInt(dateSplit[1])) + " " + dateSplit[2];
+    }
+
+    private static String formatDayNumber(int day) {
+        if (day < 10)
+            return "0" + day;
+        else
+            return String.valueOf(day);
+    }
+
+    private static String formatMonthText(int month) {
+        String monthText;
+        switch (month) {
+            case 2:
+                monthText = "Feb";
+                break;
+            case 3:
+                monthText = "Mar";
+                break;
+            case 4:
+                monthText = "Apr";
+                break;
+            case 5:
+                monthText = "May";
+                break;
+            case 6:
+                monthText = "Jun";
+                break;
+            case 7:
+                monthText = "Jul";
+                break;
+            case 8:
+                monthText = "Aug";
+                break;
+            case 9:
+                monthText = "Sep";
+                break;
+            case 10:
+                monthText = "Oct";
+                break;
+            case 11:
+                monthText = "Nov";
+                break;
+            case 12:
+                monthText = "Dec";
+                break;
+            default:
+                monthText = "Jan";
+                break;
+        }
+        return monthText;
+    }
+}
