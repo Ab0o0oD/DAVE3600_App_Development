b281865ae67f9a5be3fc5a79731fe25f2a34918e
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-25 12:34:33 +0100

    Added commands for adding mock values while testing deletion

---
+++ Room4You/database/mockValues.sql
@@ -0,0 +1,6 @@
+INSERT INTO Buildings values('', 'Pilestredet 103', 4, '59.920050, 10.736118');
+INSERT INTO Rooms values('', 7, 'bæsj', 3, '59.919444, 10.735256'); 
+INSERT INTO Rooms values('', 7, 'tiss', 3, '59.919441, 10.734835'); 
+INSERT INTO Bookings values('', 15, 7, 'Martina Førre', '14:00', '15:00', '23. Nov 2019');
+INSERT INTO Bookings values('', 15, 7, 'Nikita Petrovs', '14:00', '15:00', '02. Dec 2019');
+INSERT INTO Bookings values('', 16, 7, 'Signe Eide', '14:00', '15:00', '23. Nov 2019');
