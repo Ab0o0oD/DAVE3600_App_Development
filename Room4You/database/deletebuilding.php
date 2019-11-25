716edcd3418c1e443c0cca5dfa4f8fb96fefde9b
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-25 12:36:17 +0100

    Adjusted script to delete bookings and rooms belonging to the building

--- Room4You/database/deletebuilding.php
+++ Room4You/database/deletebuilding.php
@@ -4,10 +4,24 @@ $con=mysqli_connect('student.cs.hioa.no','s306631','','s306631');
 $in1=$_REQUEST['buildingId'];
 $buildingId=(int)$in1;
 
-$sql="DELETE FROM Buildings WHERE BuildingID = '$buildingId'";
+$sqlBooking = "DELETE FROM Bookings WHERE BuildingID = (SELECT BuildingID FROM Buildings WHERE BuildingID = '$buildingId')";
+$sqlRoom = "DELETE FROM Rooms WHERE BuildingID = (SELECT BuildingID FROM Buildings WHERE BuildingID = '$buildingId')";
+$sqlBuilding="DELETE FROM Buildings WHERE BuildingID = '$buildingId'";
 
-if ($con->query($sql)=== TRUE) {
-	echo "Building Deleted Successfully"; 
+if ($con->query($sqlBooking)=== TRUE) {
+	echo "Bookings Deleted Successfully";
+} else {
+	echo "Error deleting Bookings: " . $con->error;
+}
+
+if ($con->query($sqlRoom)=== TRUE) {
+	echo "Rooms Deleted Successfully";
+} else {
+	echo "Error deleting Rooms: " . $con->error;
+}
+
+if ($con->query($sqlBuilding)=== TRUE) {
+	echo "Building Deleted Successfully";
 } else {
 	echo "Error deleting Building: " . $con->error;
 }
