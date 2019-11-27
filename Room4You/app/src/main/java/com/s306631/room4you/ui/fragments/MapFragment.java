b9c25676716496e7e5a05e7c55ed27cdd900013d
Fredrik Pedersen <fredrikhp@gmail.com>
2019-11-27 10:46:49 +0100

    Removed unused imports

--- Room4You/app/src/main/java/com/s306631/room4you/ui/fragments/MapFragment.java
+++ Room4You/app/src/main/java/com/s306631/room4you/ui/fragments/MapFragment.java
@@ -1,28 +1,21 @@
 package com.s306631.room4you.ui.fragments;
 
 import android.content.Context;
-import android.location.Location;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.Button;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
 
-import com.google.android.gms.location.FusedLocationProviderClient;
-import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.MapView;
 import com.google.android.gms.maps.MapsInitializer;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.tasks.OnCompleteListener;
-import com.google.android.gms.tasks.Task;
 import com.s306631.room4you.R;
 import com.s306631.room4you.util.CoordinatesParser;
 
